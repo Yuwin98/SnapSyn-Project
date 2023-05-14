8036b82b039d0ee55be7aeca3c7e869cadbd2c7a
Yuwin Sapumana <yuwinsapumana212@gmail.com>
2023-05-14 10:29:16 +0530

    - Setup Custom colors and typography for the custom app theme

--- app/src/main/java/com/yuwin/snapsyn/MainActivity.kt
+++ app/src/main/java/com/yuwin/snapsyn/MainActivity.kt
@@ -4,43 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.yuwin.snapsyn.ui.theme.SnapSynTheme
+import com.yuwin.snapsyn.ui.theme.AppTheme
+import com.yuwin.snapsyn.ui.theme.SnapSyn
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SnapSynTheme {
-                // A surface container using the 'background' color from the theme
+            AppTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = SnapSyn.colors.primary,
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SnapSynTheme {
-        Greeting("Android")
-    }
-}
