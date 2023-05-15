1ce6e2b472d16ee895295d75bea12fea2af9d597
Yuwin Sapumana <yuwinsapumana212@gmail.com>
2023-05-15 10:29:58 +0530

    - Added app Icon

--- app/src/main/java/com/yuwin/snapsyn/MainActivity.kt
+++ app/src/main/java/com/yuwin/snapsyn/MainActivity.kt
@@ -3,7 +3,6 @@ package com.yuwin.snapsyn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.rememberNavController
 import com.yuwin.snapsyn.nav.AppNavHost
 import com.yuwin.snapsyn.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
