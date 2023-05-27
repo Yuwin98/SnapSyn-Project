41569d5f585cc7a75aa63f48cbaf1965b23ade9b
Yuwin Sapumana <yuwinsapumana212@gmail.com>
2023-05-27 15:40:47 +0530

    - Added logo into home screen - Changed Home Screen Background color

--- app/src/main/java/com/yuwin/snapsyn/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/yuwin/snapsyn/ui/screens/home/HomeScreen.kt
@@ -1,34 +1,41 @@
 package com.yuwin.snapsyn.ui.screens.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.yuwin.snapsyn.R
 import com.yuwin.snapsyn.ui.theme.SnapSyn
 
 @Composable
 fun HomeScreen() {
 
-    Surface(
-        color = SnapSyn.colors.secondary,
+    Box(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .background(color = SnapSyn.colors.background)
     ) {
-        Box {
-            Text(
-                text = "HOME",
-                style = SnapSyn.typography.body,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .align(Alignment.Center)
-            )
-        }
+
+        Icon(
+            painter = painterResource(id = R.drawable.snapsyn_text_logo),
+            contentDescription = "Text Logo",
+            tint = SnapSyn.colors.white,
+            modifier = Modifier.padding(32.dp).size(width = 150.dp, height = 60.dp)
+        )
 
     }
-
 }
--- app/src/main/java/com/yuwin/snapsyn/ui/theme/Color.kt
+++ app/src/main/java/com/yuwin/snapsyn/ui/theme/Color.kt
@@ -9,21 +9,30 @@ data class CustomColors(
     val primary: Color,
     val secondary: Color,
     val accent: Color,
-    val background: Color
+    val background: Color,
+    val gradient_start: Color,
+    val gradient_end: Color,
+    val white: Color
 )
 
 val lightColors = CustomColors(
     primary = Color(0xFFEF476F),
     secondary = Color(0xFF06D6A0),
     accent = Color(0xFFFFD166),
-    background = Color(0xFF222222)
+    background = Color(0xFF222222),
+    gradient_start = Color(0xFFFF9171),
+    gradient_end = Color(0xFFFF005C),
+    white = Color(0xFFFFFFFF)
 )
 
 val darkColors = CustomColors(
     primary = Color(0xFFEF476F),
     secondary = Color(0xFF06D6A0),
     accent = Color(0xFFFFD166),
-    background = Color(0xFF222222)
+    background = Color(0xFF222222),
+    gradient_start = Color(0xFFFF9171),
+    gradient_end = Color(0xFFFF005C),
+    white = Color(0xFFFFFFFF)
 )
 
 internal val LocalColors = staticCompositionLocalOf { lightColors }
